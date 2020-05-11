0b4ddb71d36d8a42298a28400e0acd3a0e3a910a
cyx35 <799737179@qq.com>
2020-05-11 16:00:24 +0800

    Spring Cloud Config 分布式配置中心

---
+++ spring-cloud-config/config-client/src/test/java/com/cxy35/sample/springcloud/config/client/ConfigClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cxy35.sample.springcloud.config.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ spring-cloud-config/config-server/src/test/java/com/cxy35/sample/springcloud/config/server/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cxy35.sample.springcloud.config.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
